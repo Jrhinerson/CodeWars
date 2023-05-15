0edbde377c9941e81b792ccbcd4f5b1028eee5af
Jrhinerson <jordan.rhinerson@gmail.com>
2023-05-15 16:16:08 -0400

    first commit

---
+++ CodeWars.js
@@ -0,0 +1,7 @@
+//5/15/23
+    
+    //Messi is a soccer player with goals in 3 leagues. Complete the function to return his total number of goals in all three leagues.
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
