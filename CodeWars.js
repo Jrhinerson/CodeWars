d52bcfcc5de38fd4e0f9514dc7fc9ce19ced67ae
Jrhinerson <jordan.rhinerson@gmail.com>
2023-05-20 15:49:32 -0400

    5/20

--- CodeWars.js
+++ CodeWars.js
@@ -5,3 +5,23 @@
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
   }
+
+//5/20/23
+
+    //We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+function stringToNumber(str) {
+    return Number(str)
+  }
+
+    //Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
+
+  //Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+function repeatStr (n, s) {
+    return s.repeat(n)
+  }
