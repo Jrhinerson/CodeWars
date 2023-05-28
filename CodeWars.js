92978ab7de4a8e76387ad3f9d837010665edf463
Jrhinerson <jordan.rhinerson@gmail.com>
2023-05-28 03:54:30 -0400

    5/28/23 Binary Challenge

--- CodeWars.js
+++ CodeWars.js
@@ -97,10 +97,26 @@ function DNAStrand(dna){
   }
   return compDNAStrand
 }
+
 //Write a function that takes in a string of onew or more words, and returns the same string, but with all fiver or more letter words reversed. Strings passed in will consist of only letters and spaces. Spaces will be uncluded only  when more than one word is present.
 
 function spinWords(words){
   return words.split(' ').map(function (word) {
     return (word.length > 4) ? word.split('').reverse().join('') : word;
   }).join(' ');
+}
+
+//5/28/23
+
+//Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+//Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case.
+
+function countBits(n) {
+  let binary = n.toString(2)
+  let numCount = 0
+  for (i = 0; i < binary.length; i++) {
+    if (binary[i] == 1) {
+      numCount += 1
+    }
+  } return numCount
 }
